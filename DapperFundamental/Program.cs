ca242e5e1f6837c3b07144eedb59b5774b7f0e7e
theosihotang06 <theosihotang06@gmail.com>
2024-05-25 23:28:47 +0700

    lambda expression

--- DapperFundamental/Program.cs
+++ DapperFundamental/Program.cs
@@ -7,22 +7,6 @@ public class Program
 {
     public static void Main(string[] args)
     {
-       /*
-        * Lambda expression => didefinisikan sebagai anonymus function yang artinya method/ function tanpa nama
-        *
-        * ada 2 tipe lambda Expression :
-        * - lambda expression : dimana bodynya sebagai expression
-        * - statement lambda : dimana memiliki block code sebagai bodynya
-        */
-
-       // lambda expression
-       var square = (int x) => x * x;
-       Console.WriteLine(square(10));
-       
-       // statement lambda
-       var HelloWorld = (string firstName, string lastName) =>
-       {
-           Console.WriteLine($"nama saya {firstName}");
-       };
+      
     }
 }
